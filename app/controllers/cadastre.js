073e1c4515e856cf38874ebd4b25d294dcc6a5ed
Jefferson Gomes <jefferson.g.silva@hotmail.com>
2018-07-24 03:46:55 -0300

    Validação do formulário de cadastro criada

--- app/controllers/cadastre.js
+++ app/controllers/cadastre.js
@@ -1,3 +1,22 @@
 module.exports.cadastre = (app, req, res) => {
-    res.render("cadastre");
+    res.render("cadastre", { valid : {} });
+}
+
+module.exports.register = (app, req, res) => {
+
+    var formData = req.body;
+
+    req.assert("user", "Nome do usuário não pode ficar vazio").notEmpty();
+    req.assert("nickname", "Nickname não pode ficar vazio").notEmpty();
+    req.assert("password", "Senha não pode ficar vazia").notEmpty();
+
+    var error = req.validationErrors();
+
+    if(error){
+        res.render("cadastre", { valid : error });
+    }
+
+    var connection = app.config.dbConnection;
+    var UserDAO = new app.app.models.UserDAO(connection);
+    
 }
--- app/routes/cadastre.js
+++ app/routes/cadastre.js
@@ -1,5 +1,9 @@
 module.exports = (app) => {
     app.get("/cadastro", (req, res) => {
         app.app.controllers.cadastre.cadastre(app, req, res);
-    })
+    });
+
+    app.post("/register", (req, res) => {
+        app.app.controllers.cadastre.register(app, req, res);
+    });
 }
