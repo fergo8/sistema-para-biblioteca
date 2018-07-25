72568b78ddaff0cb7d0abe67856df7fc9e228f53
Jefferson Gomes <jefferson.g.silva@hotmail.com>
2018-07-25 19:12:32 -0300

    Formulário de cadastro de livro criado com validação de dados

--- app/controllers/book_cadastre.js
+++ app/controllers/book_cadastre.js
@@ -1,3 +1,20 @@
 module.exports.book_cadastre = (app, req, res) => {
-    res.render("book_cadastre");
+    res.render("book_cadastre", { valid : {} });
+}
+
+module.exports.book_register = (app, req, res) => {
+
+    var formData = req.body;
+
+    req.assert("title", "Título do livro não foi definido").notEmpty();
+    req.assert("author", "Nome do autor não foi definido").notEmpty();
+    req.assert("genre", "Gênero não foi definido").notEmpty();
+    req.assert("pages", "Quantidade de páginas não foi definida").notEmpty();
+    req.assert("status", "Status de leitura não foi definido").notEmpty();
+
+    var error = req.validationErrors();
+
+    if(error){
+        res.render("book_cadastre", { valid : error })
+    }
 }
--- app/routes/book_cadastre.js
+++ app/routes/book_cadastre.js
@@ -2,4 +2,8 @@ module.exports = (app) => {
     app.get("/cadastro_de_livro", (req, res) => {
         app.app.controllers.book_cadastre.book_cadastre(app, req, res);
     });
+
+    app.post("/book_register", (req, res) => {
+        app.app.controllers.book_cadastre.book_register(app, req, res);
+    });
 }
