eb220c412c33196c44fcc777e12f36fbda32c970
Jefferson Gomes <jefferson.g.silva@hotmail.com>
2018-07-26 18:47:12 -0300

    Correção de bug na validação do formulário de cadastro de livros

--- app/controllers/book_cadastre.js
+++ app/controllers/book_cadastre.js
@@ -1,5 +1,5 @@
 module.exports.book_cadastre = (app, req, res) => {
-    res.render("book_cadastre", { valid : {} });
+    res.render("book_cadastre", { bookValid : {}, msg : {} });
 }
 
 module.exports.book_register = (app, req, res) => {
@@ -8,14 +8,12 @@ module.exports.book_register = (app, req, res) => {
 
     req.assert("title", "Título do livro não foi definido").notEmpty();
     req.assert("author", "Nome do autor não foi definido").notEmpty();
-    req.assert("genre", "Gênero não foi definido").notEmpty();
-    req.assert("pages", "Quantidade de páginas não foi definida").notEmpty();
-    req.assert("status", "Status de leitura não foi definido").notEmpty();
 
     var error = req.validationErrors();
 
     if(error){
-        res.render("book_cadastre", { valid : error });
+        res.render("book_cadastre", { bookValid : error, msg : {} });
+        return;
     }
 
     var connection = app.config.dbConnection;
@@ -23,5 +21,5 @@ module.exports.book_register = (app, req, res) => {
 
     LibraryDAO.insertBook(formData);
 
-    res.render("book_cadastre", { valid : {} });
+    res.render("book_cadastre", { bookValid : {}, msg : "Livro cadastrado com sucesso" });
 }
